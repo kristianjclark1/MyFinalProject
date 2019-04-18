ca76fb107de155ca2dcdb188c92d34d6d3b862db
kclark1 <saidar72@yahoo.com>
2019-04-17 23:11:11 -0500

    Added model and list of fields

---
+++ ParkingLotApp/ParkingLotApp.Model/Model/ParkingLot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ParkingLotApp.Models
+{
+    public class ParkingLot
+    {
+        public string Address { get; set; }
+        public string Location { get; set; }
+        public string Floor { get; set; }
+        public int Spaces { get; set; }
+        public int Size { get; set; }
+        public bool Handicap { get; set; }
+    }
+}
---
+++ ParkingLotApp/ParkingLotApp/Controllers/ParkingLotController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkingLotApp.Models;
+
+namespace ParkingLotApp.WebUI.Controllers
+{
+    public class ParkingLotController : Controller
+    {
+        private List<ParkingLot> ParkingLots = new List<ParkingLot>();
+
+        //parkinglot/index
+        public IActionResult Index()
+        {
+            return View(ParkingLots);
+        }
+
+        //GET parkinglot/add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ParkingLot newParkingLot)
+        {
+            ParkingLots.Add(newParkingLot);
+
+            return View(nameof(Index), ParkingLots);
+        }
+    }
+}
