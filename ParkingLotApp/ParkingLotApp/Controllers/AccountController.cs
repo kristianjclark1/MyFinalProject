7ac3600e4fe165149b07bbeabec6bef0e77ec974
kclark1 <saidar72@yahoo.com>
2019-05-24 00:26:33 -0500

    Created Register User page, account controller page and their view pages.

---
+++ ParkingLotApp/ParkingLotApp/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ParkingLotApp.Controllers;
+using ParkingLotApp.Domain.Model;
+using ParkingLotApp.Models;
+
+namespace ParkingLotApp.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager,
+            SignInManager<AppUser>signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            //register the user
+            if (ModelState.IsValid)
+            {
+                var newUser = new AppUser
+                {
+                    UserName = vm.Email,
+                    NormalizedUserName = vm.Email.ToUpper(),
+                    Email = vm.Email,
+                    NormalizedEmail = vm.Email.ToUpper()
+                };
+
+                 var result = await _userManager.CreateAsync(newUser, vm.Password);
+
+                if (result.Succeeded)
+                {
+                    // new user got created
+                    //we can login the user
+                    await _signInManager.SignInAsync(newUser, false);
+                    //send user to the right page (redirect)
+                    return RedirectToAction("Index","HomeController"); //home/index
+
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            // sending back the error to the view (register form)
+            return View(vm);
+        }
+    }
+}
---
+++ ParkingLotApp/ParkingLotApp/Models/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingLotApp.Models
+{
+    public class RegisterViewModel
+    {
+        [EmailAddress, Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), Required]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Required]
+        [Compare(nameof(Password), ErrorMessage ="Make sure your password matches")]
+        public string ConfirmPassword { get; set; }
+    }
+}
