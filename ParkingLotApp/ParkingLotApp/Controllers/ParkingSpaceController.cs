30995f0d0dda6f3b450b02f4333570c99975b7a4
kclark1 <saidar72@yahoo.com>
2019-07-16 00:17:30 -0500

    Added to ParkingSpaceController, the Add.

--- ParkingLotApp/ParkingLotApp/Controllers/ParkingSpaceController.cs
+++ ParkingLotApp/ParkingLotApp/Controllers/ParkingSpaceController.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ParkingLotApp.Domain.Model;
 
 namespace ParkingLotApp.WebUI.Controllers
 {
     public class ParkingSpaceController : Controller
     {
         public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ParkingSpace newParkingSpace)
         {
             return View();
         }
